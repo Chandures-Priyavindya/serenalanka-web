ea2296c81393bb8c7bd09ba237613ff0ddc1ea0f
Chandures-Priyavindya <145960853+Chandures-Priyavindya@users.noreply.github.com>
2024-12-21 21:29:04 +0530

    az

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import './home.scss'
+import React from "react";
+import "./home.scss";
+import img from "../../assets/sea2.avif";
+import { FaLocationDot } from "react-icons/fa6";
 
 const Home = () => {
   return (
-    <div>
-      Home
-    </div>
-  )
-}
+    <section className="home">
+      <div className="overlay"></div>
 
-export default Home
+      <img src={img} type="sea2/avif" alt="A scenic beach view" />
+
+      <div className="homeContent container">
+        <div className="textDiv">
+          <span className="smallText">Our Packages</span>
+          <h1 className="homeTitle">Search your Holiday</h1>
+        </div>
+        <div className="cardDiv grid">
+          <div className="destinstionInput">
+            <label htmlFor="city">Search your destinstion:</label>
+            <div className="input flex">
+              <input type="text" placeholder="Enter name here....." />
+              <FaLocationDot className="icon" />
+            </div>
+          </div>
+          <div className="dateInput">
+            <label htmlFor="date">Select your date:</label>
+            <div className="input flex">
+              <input type="date" />
+            </div>
+          </div>
+          <div className="priceInput">
+            <div className="label_total flex">
+              <label htmlFor="price">Max price:</label>
+              <h3 className="total">$3000</h3>
+            </div>
+            <div className="input flex">
+              <input type="range" max="3000" min="1000"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
